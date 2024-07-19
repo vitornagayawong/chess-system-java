f821d319471a5442ca8bc2a46d604f68b40d0277
Vitor Nagaya Wong <vnwgithub@gmail.com>
2024-07-19 17:01:38 -0300

    Chess layer and print the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
+		ChessMatch chessmatch = new ChessMatch();
+		UI.printBoard(chessmatch.getPieces());
 	}
 
 }
--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,5 +1,24 @@
 package boardgame;
 
 public class Piece {
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;		
+	}
 
+	//SOMENTE CLASSES DENTRO DO MESMO PACOTE E SUBCLASSES É QUE VAO PODER ACESSAR O TABULEIRO DE UMA PEÇA
+	protected Board getBoard() {
+		return board;
+	}
+
+	/* NAO PODE PERMITIR QUE O TABULEIRO SEJA ALTERADO
+	public void setBoard(Board board) {
+		this.board = board;
+	}
+	*/
+	
+	
 }
